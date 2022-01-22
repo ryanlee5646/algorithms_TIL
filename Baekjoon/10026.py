6b4abdf8551c08b196e29c1848f709cdc1933cc1
ryanlee <ryanlee5646@gmail.com>
2022-01-23 00:20:50 +0900

    20220123 algorithm_TIL

--- Baekjoon/10026.py
+++ Baekjoon/10026.py
@@ -1,8 +1,6 @@
-# 적록색약
-
 from collections import deque
 
-def bfs(x, y):
+def BFS(x, y):
     queue.append([x, y])
     visit[x][y] = cnt
     while queue:
@@ -16,35 +14,32 @@ def bfs(x, y):
                     visit[nx][ny] = 1
 
 N = int(input())
-data = [list(map(str, input()) for _ in range(N))]
+data = [list(map(str, input())) for _ in range(N)]
 visit = [[0]*N for _ in range(N)]
+queue = deque()
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-queue = deque()
 
-# 일반사람
 cnt = 0
 for i in range(N):
     for j in range(N):
         if visit[i][j] == 0:
-            bfs(i, j)
+            BFS(i, j)
             cnt += 1
 print(cnt, end=' ')
-
-# 적록색맹
+print(visit)
 for i in range(N):
     for j in range(N):
         if data[i][j] == 'R':
             data[i][j] = 'G'
-            
 visit = [[0]*N for _ in range(N)]
 
 cnt = 0
 for i in range(N):
     for j in range(N):
         if visit[i][j] == 0:
-            bfs(i, j)
+            BFS(i, j)
             cnt += 1
 print(cnt)
-
+print(visit)
