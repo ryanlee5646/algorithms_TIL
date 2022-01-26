db57a20a9dff93acea503be3c1a9ac61dc66e44b
Gyujin Lee <ryanlee5646@gmail.com>
2022-01-26 23:01:19 +0900

    20210126 algorithm_TIL

--- Baekjoon/1260.py
+++ Baekjoon/1260.py
@@ -3,15 +3,15 @@ from collections import deque
 import sys
 read = sys.stdin.readlines
 
-N, line, start = map(int, read().split())
+N, line, start = map(int, sys.stdin.readlines().split())
 
 data = [[0] * (N+1) for _ in range(N+1)]
 
 dfs_visit = [0] * (N+1) 
 bfs_visit = [0] * (N+1)
 
-for _ in range(line):
-    x,y = map(int, read().split())
-    data[x][y] = data[y][x] = 1
+# for _ in range(line):
+#     x,y = map(int, read().split())
+#     data[x][y] = data[y][x] = 1
 
 print(data)
---
+++ Baekjoon/1780.py
@@ -0,0 +1,8 @@
+# 종이의 개수
+import sys
+read = sys.stdin.readline
+
+N = int(input())
+data = [list(map(int, read().split())) for _ in range(N)]
+
+print(data)
