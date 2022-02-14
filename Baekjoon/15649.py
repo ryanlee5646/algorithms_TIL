c96ca4044ca683b2065d8ccb1d689ade3743d3f5
ryanlee <ryanlee5646@gmail.com>
2022-02-15 00:12:50 +0900

    20220214 algorithm_TIL

--- Baekjoon/15649.py
+++ Baekjoon/15649.py
@@ -2,16 +2,16 @@
 from sys import stdin
 input = stdin.readline
 
+## 풀이 1 ##
 # 순열 출력값
 tmp = []
 def permutation():
     if len(tmp) == M:
         print(' '.join(map(str, tmp)))
-    
+        return
     for i in data:
         if i in tmp:
             continue
-        print(i)
         tmp.append(i)
         permutation()
         tmp.pop()    
@@ -19,3 +19,34 @@ def permutation():
 N, M = map(int, input().split())
 data = [i for i in range(1, N+1)]
 permutation()
+
+## 풀이 2 ##
+# 순열 출력값
+def permutation(depth, N, M):
+    if len(tmp) == M:
+        print(' '.join(map(str, tmp)))
+        return
+    for i in range(len(data)):
+        if not visited[i]:
+           visited[i] = 1
+           tmp.append(data[i])
+           permutation(depth+1, N, M)
+           visited[i] = 0 # 끝까지 탐색 했으면 빼주기
+           tmp.pop()
+ 
+N, M = map(int, input().split())
+data = [i for i in range(1, N+1)]
+visited = [0] * N
+tmp = []
+permutation(0, N, M)
+
+## 풀이3 ##
+# itertools이용 permutation 
+from itertools import permutations
+N, M = map(int, input().split())
+data = [i for i in range(1, N+1)]
+
+p = list(permutations(data,M))
+
+for i in p:
+    print(' '.join(map(str, i)))
