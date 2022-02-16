aa048ea591a7cec61f61b04f82c5b994494ccdb1
Gyujin Lee <ryanlee5646@gmail.com>
2022-02-16 13:17:21 +0900

    20220216 algorithm_TIL

--- Baekjoon/15654.py
+++ Baekjoon/15654.py
@@ -1,7 +1,24 @@
 # N과 M(5)
+from itertools import permutations
 from sys import stdin
 input = stdin.readline
 
+def permutation(start):
+    if len(tmp) == M:
+        print(' '.join(map(str,tmp)))
+        return
+    for i in range(len(data)):
+        if not visited[i]:
+            visited[i] = 1
+            tmp.append(data[i])
+            permutation(i)
+            visited[i] = 0
+            tmp.pop() 
+     
 N, M = map(int, input().split())
 data = list(map(int, input().split()))
+data.sort()
+tmp = []
+visited = [0] * N
 
+permutation(0)
---
+++ Baekjoon/15655.py
@@ -0,0 +1,4 @@
+# N과 M (6)
+from sys import stdin
+input = stdin.readline
+
