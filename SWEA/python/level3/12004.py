8469b2f44f9fa0e8fe584d7882eb5ebc049bdb87
ryanlee <ryanlee5646@gmail.com>
2022-03-08 00:25:57 +0900

    20220308 algorithm TIL

--- SWEA/python/level3/12004.py
+++ SWEA/python/level3/12004.py
@@ -2,29 +2,32 @@
 from sys import stdin
 input = stdin.readline
 
-def combination(depth):
-    global count
-    if count > 0:
-        print(f"#{t} Yes")
-        return
-    if depth == 2:    
-        if (tmp[0] * tmp[1]) == num:
-            count+=1
-    
-    for i in range(depth, len(num_list)):
-        tmp.append(num_list[i])
-        combination(depth+1)
-        tmp.pop()
-        
 T = int(input())
+
+# array = [False] * 101 # 왜 100으로 하면 안되지? N범위가 100인데?
+# for i in range(1, 10):
+#         for j in range(i, 10):
+#             array[i*j] = True
+
+# for t in range(1, T+1):
+#     num = int(input())
+#     if array[num] == True:
+#         print(f"#{t} Yes")
+#     else:
+#         print(f"#{t} No")
+
 
 for t in range(1, T+1):
     num = int(input())
-    num_list = [1,2,3,4,5,6,7,8,9]
-    tmp = []
-    count = 0
-    combination(0)
-    if count == 0:
+    flag = True
+    for i in range(1, 10):
+        for j in range(i, 10):
+            if (i*j) == num:
+                print(f"#{t} Yes")
+                flag = False
+                break
+        if(flag == False):
+            break
+    else:
         print(f"#{t} No")
-    # else: 
-    #     print(f"#{t} Yes")
+    
