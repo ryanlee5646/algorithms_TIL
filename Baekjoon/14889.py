f8233fdd20863433b5db535577b3b0989fa794da
ryanlee <ryanlee5646@gmail.com>
2022-03-13 03:20:36 +0900

    20220313 algorithm tIL

--- Baekjoon/14889.py
+++ Baekjoon/14889.py
@@ -1,46 +1,40 @@
 # 스타트와 링크(시간초과 단계를 줄여야할듯)
 from sys import stdin
-from itertools import permutations
+from itertools import combinations
 input = stdin.readline
-
-def min_value_cal(start, link):
-    global min_value
-    # 각팀의 순열 구하기
-    start_list = list(permutations(start, 2))
-    link_list = list(permutations(link, 2))
-    # 각각 팀의 값 구하기
-    start_value = sum(S[i][j] for i, j in start_list)
-    link_value = sum(S[i][j] for i, j in link_list)
-    # 각 팀의 차이값 구하기
-    result = abs(start_value - link_value)
-    # 최소값 구하기
-    min_value = min(min_value, result)
-    return 
-
-def combination(depth):
-    if depth == int(N/2):
-        start = []
-        link = []
-        for i, j in enumerate(visited):
-            if j == 1:
-                start.append(i)
-            else:
-                link.append(i)
-                
-        min_value_cal(start, link)
-        
-    for i in range(depth, N):
-        if not visited[i]:
-            visited[i] = 1
-            combination(depth+1)
-            visited[i] = 0
 
 N = int(input())
 S = [list(map(int, input().split())) for _ in range(N)]
 data = [i for i in range(N)]
 min_value = 1e9
-visited = [0] * N
-tmp = []
 
-combination(0)
-print(min_value)
+for i in zip(*S):
+    print(i)
+
+# # 스타트팀 조합 구하기
+# for combination in combinations(data, int(N/2)):
+#     visited = [0] * N
+#     start_team = []
+#     link_team = []
+    
+#     # 스타트팀 체크하기
+#     for i in combination:
+#         visited[i] = 1
+#         start_team.append(i)
+#     # 링크팀 구하기    
+#     for i in range(N):
+#         if not visited[i]:
+#             link_team.append(i)
+    
+#     start_team_score = 0
+#     link_team_score = 0
+    
+#     for i in range(int(N/2)):
+#         for j in range(int(N/2)):
+#             if i != j:
+#                 start_team_score += S[start_team[i]][start_team[j]]
+#                 link_team_score += S[link_team[i]][link_team[j]]
+
+#     min_value = min(min_value, abs(start_team_score - link_team_score))
+
+# print(min_value)
