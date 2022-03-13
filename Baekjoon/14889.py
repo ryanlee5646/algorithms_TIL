57964d47c65330ac47204d4b13d8575cba284405
ryanlee <ryanlee5646@gmail.com>
2022-03-14 00:18:20 +0900

    20220313 algorithm_TIL

--- Baekjoon/14889.py
+++ Baekjoon/14889.py
@@ -10,65 +10,65 @@ min_score = 1e9
 
 ### 풀이1 ###
 # 점수 계산
-# def cal_score(start_team, link_team):
-#     start_team_score = 0
-#     link_team_score = 0
+def cal_score(start_team, link_team):
+    start_team_score = 0
+    link_team_score = 0
     
-#     for i in range(len(start_team)):
-#         for j in range(len(start_team)):
-#             start_team_score += S[start_team[i]][start_team[j]]
-#             link_team_score += S[link_team[i]][link_team[j]]
+    for i in range(len(start_team)):
+        for j in range(len(start_team)):
+            start_team_score += S[start_team[i]][start_team[j]]
+            link_team_score += S[link_team[i]][link_team[j]]
 
-#     return abs(start_team_score - link_team_score)
+    return abs(start_team_score - link_team_score)
 
-# def combination(start_team, depth, start):
-#     global min_score
-#     # 링크팀 구하기
-#     if depth == N//2:
-#         link_team = []
-#         for i in range(N):
-#             if i not in start_team:
-#                 link_team.append(i)
+def combination(start_team, depth, start):
+    global min_score
+    # 링크팀 구하기
+    if depth == N//2:
+        link_team = []
+        for i in range(N):
+            if i not in start_team:
+                link_team.append(i)
 
-#         tmp_score = cal_score(start_team, link_team)
-#         min_score = min(min_score, tmp_score)
-#         return
+        tmp_score = cal_score(start_team, link_team)
+        min_score = min(min_score, tmp_score)
+        return
     
-#     # 스타트팀 구하기
-#     for i in range(start, N):
-#         if i not in start_team:
-#             start_team.append(i)
-#             combination(start_team, depth+1, i+1)
-#             start_team.pop()
+    # 스타트팀 구하기
+    for i in range(start, N):
+        if i not in start_team:
+            start_team.append(i)
+            combination(start_team, depth+1, i+1)
+            start_team.pop()
 
-# combination([], 0, 0)
+combination([], 0, 0)
 
-# print(min_score)
+print(min_score)
 
 ### 풀이2 (라이브러리 조합 + reversed) ###
 
 # 점수 계산
-# def cal_score(start_team, link_team):
-#     start_team_score = 0
-#     link_team_score = 0
+def cal_score(start_team, link_team):
+    start_team_score = 0
+    link_team_score = 0
     
-#     for i in range(len(start_team)):
-#         for j in range(len(start_team)):
-#             start_team_score += S[start_team[i]][start_team[j]]
-#             link_team_score += S[link_team[i]][link_team[j]]
+    for i in range(len(start_team)):
+        for j in range(len(start_team)):
+            start_team_score += S[start_team[i]][start_team[j]]
+            link_team_score += S[link_team[i]][link_team[j]]
 
-#     return abs(start_team_score - link_team_score)
+    return abs(start_team_score - link_team_score)
 
 # # 스타트팀 / 링크팀 구하기
-# start_combi = list(combinations(data, int(N/2)))
-# link_combi = list(reversed(start_combi)) # 스타트님 조합을 뒤집으면 링크팀이 됌
-# # start_combi: [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
-# # link_combi: [(2, 3), (1, 3), (1, 2), (0, 3), (0, 2), (0, 1)]
+start_combi = list(combinations(data, int(N/2)))
+link_combi = list(reversed(start_combi)) # 스타트님 조합을 뒤집으면 링크팀이 됌
+# start_combi: [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
+# link_combi: [(2, 3), (1, 3), (1, 2), (0, 3), (0, 2), (0, 1)]
 
-# for i in range(len(start_combi) // 2):
-#     min_score = min(min_score, cal_score(start_combi[i], link_combi[i]))
+for i in range(len(start_combi) // 2):
+    min_score = min(min_score, cal_score(start_combi[i], link_combi[i]))
 
-# print(min_score)
+print(min_score)
 
 
 ### 풀이3 (zip사용) ###
