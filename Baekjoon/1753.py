d84f0cb549b4e881e2ec0f1ee6970d8fb5a47c97
Gyujin Lee <ryanlee5646@gmail.com>
2022-03-15 01:03:47 +0900

    20220315 algorithm TIL

--- Baekjoon/1753.py
+++ Baekjoon/1753.py
@@ -1,54 +1,53 @@
-# 최단경로
-from sys import stdin
-input = stdin.readline
+import sys
+import heapq
+input = sys.stdin.readline
+
+# 정점의 개수 n, 간선의 개수 m
+n, m = map(int, input().split())
+
+# 시작 정점의 번호
+k = int(input())
+
+# 무한을 의미하는 INF
 INF = int(1e9)
 
+# 그래프 초기화
+graph = [[] * (n+1) for _ in range(n+1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+# 간선 정보 입력
+for _ in range(m):
+    s, e, c = map(int, input().split()) 
+    # s->e가 c비용
+    graph[s].append((e, c))
+
 def dijkstra(start):
-    # 시작 노드 초기화
+    queue = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(queue, (0, start))
     distance[start] = 0
-    visited[start] = 0
-    for i in graph[start]:
-        distance[i[0]] = i[1]
-        
-    # 시작 노드를 제외한 전체 N-1개의 노드에 대해 반복
-    for i in range(N-1):
-        # 현재 최단 거리가 가장 짧은 노드를 꺼내서 방문철
-        now = get_smallest_node()
-        visited[now] = True
-        # 현재 노드와 연결된 다른 노드를 확인
-        for j in graph[now]:
-            cost = distance[now] + j[1]
-            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
-    
-# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
-def get_smallest_node():
-    min_value = INF
-    index = 0 # 가장 최단거리가 짧은 노드(인덱스)
-    for i in range(1, N+1): 
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-    return index
-    
-# 노드수, 간선수
-N, M = map(int, input().split())
-start = int(input())
-graph = [[] for _ in range(N+1)]
-# 최단거리 테이블 모두 무한으로 초기화
-distance = [INF] * (N+1)
-# 노드방문 체크
-visited = [0] * (N+1)
 
-for i in range(M):
-    s, e, w = map(int, input().split())
-    # s(시작)에서 e(끝점)으로 가는 w(가중치)
-    graph[s].append((e,w))
+    while queue:
+        print(queue)
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(queue)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1] # 현재 노드에서 다음 노드까지 비용의 합
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(queue, (cost, i[0]))
 
-dijkstra(start)
+# 다익스트라 알고리즘을 수행
+dijkstra(k)
 
-for i in range(1, N+1):
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n+1):
     if distance[i] == INF:
         print("INF")
     else:
