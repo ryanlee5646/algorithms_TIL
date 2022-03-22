896d9eef4c8eb8f5a8c5d3f7ff7829c6cc10292f
Gyujin Lee <ryanlee5646@gmail.com>
2022-03-22 18:46:32 +0900

    20220322 algorithm TIL

--- test/test2.py
+++ test/test2.py
@@ -1,4 +1,13 @@
-def solution(n):
+def solution(n, clockwise):
+    # True: 시계방향 / False: 반시계방향
     return
 
 n = int(input())
+clockwise = input()
+data = [[0]*n for i in range(n)]
+last_num_memo = [0, 1, 1, 3]
+
+for i in range(4, 1000):
+    last_num_memo.append(last_num_memo[i-2] + last_num_memo[i-1])
+print(last_num_memo)
+solution(n, clockwise)
