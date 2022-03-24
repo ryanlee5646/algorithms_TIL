2f4a66efdc8304be207100bdd8db7e7f9396026e
ryanlee <ryanlee5646@gmail.com>
2022-03-25 01:00:09 +0900

    20220325 algorithm_TIL

--- test/test2.py
+++ test/test2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 # tc1
 n = 5
@@ -25,4 +24,3 @@ for i in range(4, 1000):
     last_num_memo.append(last_num_memo[i-2] + last_num_memo[i-1])
 print(last_num_memo)
 solution(n, clockwise)
->>>>>>> 896d9eef4c8eb8f5a8c5d3f7ff7829c6cc10292f
