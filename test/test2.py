d273ceca9073dd6359ec19e226204ec5e3def544
ryanlee <ryanlee5646@gmail.com>
2022-03-27 16:19:12 +0900

    20220327 algorithm_TIL

--- test/test2.py
+++ test/test2.py
@@ -1,6 +1,6 @@
-
+from collections import deque
 # tc1
-n = 5
+n = 4
 clockwise = True
 
 # tc2
@@ -11,16 +11,63 @@ clockwise = True
 # n = 9
 # clockwise = False
 
-def solution(n, clockwise):
-    # True: 시계방향 / False: 반시계방향
+def rotate(y, x, direction, move, num):
+    global clockwise
+    if move == 1:
+        y = y + dir_y[direction]
+        x = x + dir_x[direction]
+        data[y][x] = num
+        return
+    if move == 0:
+        return
+    
+    for i in range(move):
+        data[y][x] = num
+        y = y + dir_y[direction]
+        x = x + dir_x[direction]
+        num+=1
+    
+    if clockwise == True:
+        if direction == 0:
+            rotate(y, x, 1, move-2, num)
+        elif direction == 1:
+            rotate(y, x, 2, move-2, num)
+        elif direction == 2:
+            rotate(y, x, 3, move-2, num)
+        else:
+            rotate(y, x, 0, move-2, num)
+    
+    else:
+        if direction == 0:
+            rotate(y, x, 3, move-2, num)
+        elif direction == 1:
+            rotate(y, x, 0, move-2, num)
+        elif direction == 2:
+            rotate(y, x, 1, move-2, num)
+        else:
+            rotate(y, x, 2, move-2, num)
     return
 
-n = int(input())
-clockwise = input()
-data = [[0]*n for i in range(n)]
-last_num_memo = [0, 1, 1, 3]
+# n = int(input())
+# clockwise = input()
 
-for i in range(4, 1000):
-    last_num_memo.append(last_num_memo[i-2] + last_num_memo[i-1])
-print(last_num_memo)
-solution(n, clockwise)
+dir_y = [0, 1, 0, -1] # 오른쪽, 아래, 왼쪽, 위 (시계방향)
+dir_x = [1, 0, -1, 0]
+
+data = [[0]*n for i in range(n)]
+
+if n < 3:
+    for i in range(n):
+        for j in range(n):
+            data[i][j] = 1
+else:
+    if clockwise == True:
+        start_point = [[0, 0, 0], [0, n-1, 1], [n-1, 0, 3], [n-1, n-1, 2]]
+    else:
+        start_point = [[0, 0, 1], [0, n-1, 2], [n-1, 0, 0], [n-1, n-1, 3]]
+    
+    # True: 시계방향 / False: 반시계방향
+    for start in start_point:
+        y, x, direction = start
+        rotate(y, x, direction, n-1, 1) # y좌표, x좌표, 방향, 이동횟수, 숫자, clockwise 
+print(data)
