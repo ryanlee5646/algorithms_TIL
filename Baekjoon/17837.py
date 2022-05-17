9c71aa52e757512592a97c202ebc6eccb4332b6d
Gyujin Lee <ryanlee5646@gmail.com>
2022-05-17 19:52:47 +0900

    20220517 algorithm

--- Baekjoon/17837.py
+++ Baekjoon/17837.py
@@ -1,1 +1,10 @@
 # 새로운 게임2
+from sys import stdin
+input = stdin.readline
+
+n, k = map(int, input().split)
+graph = [list(map(int, input().split())) for _ in range(n)]
+chess_infos = [list(map(int, input().split())) for _ in range(k)]
+
+print(graph)
+print(chess_infos)
