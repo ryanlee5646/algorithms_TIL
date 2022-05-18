05eec9c16882a20f2afffff1c2ad83bf457a1a31
ryanlee <ryanlee5646@gmail.com>
2022-05-19 01:19:06 +0900

    20220519 algorithm

--- Baekjoon/17837.py
+++ Baekjoon/17837.py
@@ -5,6 +5,8 @@ input = stdin.readline
 n, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 chess_infos = [list(map(int, input().split())) for _ in range(k)]
+dx = [0, 0 , -1, 1]
+dy = [-1, 1, 0, 0]
 
 print(graph)
 print(chess_infos)
