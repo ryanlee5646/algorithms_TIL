a1f5ade3bfdf559fb6a56fef74734c2a6b47954d
Gyujin Lee <ryanlee5646@gmail.com>
2022-05-23 21:29:28 +0900

    20220523 alghrithm til

--- Baekjoon/17837.py
+++ Baekjoon/17837.py
@@ -25,16 +25,63 @@ y,x,이동방향
 '''
 
 from sys import stdin
-
 input = stdin.readline
 
-n, k = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-check_map = [[[] for _ in range(n)] for _ in range(n)]
-chess_infos = [list(map(int, input().split())) for _ in range(k)]
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
 
-print(graph)
-print(chess_infos)
-print(check_map)
+def change_direction(direction):
+    if direction == 0 or direction == 2:
+        direction += 1
+    elif direction == 1 or direction == 3:
+        direction -= 1
+    return direction
+
+
+def move(horse_num):
+    y, x, d = chess_horse[horse_num]
+    nx = x + dx[d]
+    ny = y + dy[d]
+
+    # 파란색 칸이거나 맵을 벗어난 경우
+    if 0 > nx or nx >= n or 0 > ny or ny >= n or chess_map[ny][nx] == 2:
+        d = change_direction(d)
+        # 방향을 반대로 바꿔줌
+        chess_horse[horse_num][2] = d
+        nx = x + dx[d]
+        ny = y + dy[d]
+        # 반대방향으로 이동했는데 또 파란색이거나 맵을 벗어 났으면 제자리
+        if 0 > nx or nx >= n or 0 > ny or ny >= n or chess_map[ny][nx] == 2:
+            return True
+
+
+n, k = map(int, input().split())
+chess_map = [list(map(int, input().split())) for _ in range(n)]
+horse_map = [[[] for _ in range(n)] for _ in range(n)]
+dx = [0, 0, -1, 1]  # 동 서 북 남
+dy = [1, -1, 0, 0]
+
+chess_horse = []
+
+for i in range(k):
+    y, x, d = map(int, input().split())
+    # 체스말 정보를 말 순서대로 배열에 추가
+    chess_horse.append([y-1, x-1, d-1])  # 좌표, 방향 1씩 빼줘야함
+    # 좌표에 말 추가
+    horse_map[y-1][x-1].append(i)
+
+# 턴 횟수
+turn = 0
+
+while True:
+    flag = False
+
+    if turn > 1000:
+        print(-1)
+        break
+    for i in range(k):
+        if move(i) == False:
+            flag = True
+            break
+    turn += 1
+    if flag:
+        print(turn)
+        break
