82361cac8a827cd6673e5c9fde927e417322637b
Gyujin Lee <ryanlee5646@gmail.com>
2022-07-03 01:00:23 +0900

    20220701 algorithm

--- Baekjoon/10159.py
+++ Baekjoon/10159.py
@@ -1,4 +1,7 @@
 # 저울
+'''
+비교 결과를 알 수 없는 물건의 개수를 출력
+'''
 from sys import stdin
 from collections import deque
 input = stdin.readline
@@ -15,12 +18,13 @@ def BFS(stuff):
         # now 물건을 체크 확인
         if not visited[now]:
             visited[now] = 1
-            # now는 같은 물건이 아니면서,
+            # now는 같은 물건이 아니면서, now > i 보다 크다면 stuff > i인것들 체크
             if now != stuff and graph[stuff][now] == 0:
                 graph[stuff][now] = 1
                 graph[now][stuff] = -1
 
             for i in range(1, N+1):
+                # now가 i보다 무거우면서 i를 체크한적이 없다면
                 if graph[now][i] == 1 and not visited[i]:
                     queue.append(i)
 
@@ -34,21 +38,22 @@ graph = [[0]*(N+1) for _ in range(N+1)]  # 숫자 1부터 시작하니까 N+1만
 
 for i in range(M):
     heavy, light = map(int, input().split())
-    # 무거우면 1 가벼우면 -1로 체크
+    # 무거우면 1 가벼우면 -1로 체크(미리 주어진 관계를 맵에 체크)
     graph[heavy][light] = 1
     graph[light][heavy] = -1
-
+for i in graph:
+    print(i)
 # 1번 물건부터 탐색
 for i in range(1, N+1):
     BFS(i)
 
 for i in range(1, N+1):
-    # 번호가 같은 물건 말고
+    # 자신의 번호를 제외한 물건과의 관계가 0(모르는)개수구하기
     count = 0
     for j in range(1, N+1):
         if j != i and graph[i][j] == 0:
             count += 1
-    print(count)
-
+    # print(count)
+print("---------------------")
 for i in graph:
     print(i)
