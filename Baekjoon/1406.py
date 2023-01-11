b4123bd9bd76cb42f670e178d5619833f412689c
ryanlee <ryanlee5646@gmail.com>
2023-01-12 01:13:43 +0900

    20230112 시간초과

--- Baekjoon/1406.py
+++ Baekjoon/1406.py
@@ -9,6 +9,7 @@ P $:	$라는 문자를 커서 왼쪽에 추가함
 from sys import stdin
 input = stdin.readline
 
+# 1.리스트 풀이(시간초과)
 words = list(input().strip())
 N = int(input())
 
@@ -33,3 +34,31 @@ for i in range(N):
       now += 1
 for i in words:
   print(i, end="")
+
+# 1.리스트 풀이(시간초과)
+words = input().strip()
+N = int(input())
+
+now = len(words)
+
+for i in range(N):
+
+  command = input().rstrip().replace(" ", "")
+  
+  if command[0] == "L":
+    if now > 0:
+      now -= 1
+  elif command[0] == "D":
+    if now < len(words):
+      now += 1
+  elif command[0] == "B":
+    if now == 0: # 커서 가장 왼쪽이면 패스
+      continue
+    else:
+      words = words[0:now-1] + words[now:]
+      now -= 1
+  elif command[0] == "P":
+      words = words[0:now] + command[1] + words[now:]
+      now += 1
+
+print(words)
